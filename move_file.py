38a927ed375ebe668ef287b6af9a94926191f2a2
ahanroy <86710584+ahanroy@users.noreply.github.com>
2023-05-13 21:53:28 +0530

    Update move_file.py

--- move_file.py
+++ move_file.py
@@ -4,36 +4,42 @@ import random
 
 import os
 import shutil
+import logging
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
-# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
-
-from_dir = "C:/Users/DIPANWITA/Downloads"
-
-dir_tree = {
-    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
-    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
-    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
-    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
-}
+from_dir = "C:/Users/preet/Downloads"
 
 # Event Hanlder Class
-
-class FileMovementHandler(FileSystemEventHandler):
+class FileEventHandler(FileSystemEventHandler):
 
     def on_created(self, event):
-      print(f"Hey, {event.src_path} has been created")
+        print(f"Hey, {event.src_path} has been created!")
 
     def on_deleted(self, event):
-        print(f"Oops! someone deleted {event.src_path}!")
-    def on_modified(self, event):
-        print(f"{event.src_path} is modified!")
-    def on_moved(self, event):
-        print(f"{event.src_path} is moved or renamed")
+        print(f"Oops! Someone deleted {event.src_path}!")
 
+    def on_modified(self, event):
+        print(f"Hey there!, {event.src_path} has been modified")
+    
+    def on_moved(self, event):
+        print(f"Someone moved {event.src_path} to {event.dest_path}")
+        
+
+
+# Initialize Event Handler Class
+event_handler = FileEventHandler()
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
 
 try:
     while True:
@@ -42,6 +48,3 @@ try:
 except KeyboardInterrupt:
     print("stopped!")
     observer.stop()
-
-
-    
